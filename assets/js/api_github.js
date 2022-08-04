57b0065d8db177a3a0922a88c8d3890b4236d32e
Guest 01 <guest01@alunos.sc.senac.br>
2022-08-03 22:08:39 -0300

    projeto finzalizado

---
+++ assets/js/api_github.js
@@ -0,0 +1,59 @@
+
+
+function renderiza_foto(foto) {
+    const img_foto = document.getElementById('foto');
+    img_foto.src = `${foto}`
+
+}
+
+function renderiza_nome(nome) {
+    const h1_nome = document.getElementById('nome');
+    h1_nome.innerHTML = `${nome}`
+}
+
+function renderiza_link(link) {
+    const h2_link = document.getElementById('github');
+    h2_link.href = `${link}`
+}
+
+const headers = new Headers();
+headers.append('Authorization', 'token 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', {
+    headers: headers
+});
+
+fetch("https://api.github.com/users/gusssouza")                                
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        renderiza_foto(data.avatar_url)
+        renderiza_nome(data.name)
+        renderiza_link(data.html_url)
+
+    })
+
+    .catch(error => {                                                   
+        console.error('algo deu errado na requisição', error);
+
+    }
+
+    )
+
+function repos(repositorios) {
+    const divrepo = document.getElementById('repo');
+    divrepo.innerHTML += `
+        <div id="divRepo">
+        <a href=${repositorios.html_url} class="repos" target="_blank">${repositorios.name}</a><br>
+        </div>
+        `
+}
+fetch("https://api.github.com/users/gusssouza/repos")                                
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        data.forEach(repos)
+
+
+    })
+
+
+
