38c7fc061d6ae2f09ba75af933dbc90ddab4e09d
HyperV <HyperV@JLE106D70.alunos.sc.senac.br>
2022-08-16 21:50:17 -0300

    Atualizao Sobre Mim

--- assets/js/api_github.js
+++ assets/js/api_github.js
@@ -1,6 +1,4 @@
-
-
-function renderiza_foto(foto) {
+                                                                                                                            function renderiza_foto(foto) {
     const img_foto = document.getElementById('foto');
     img_foto.src = `${foto}`
 
@@ -17,43 +15,41 @@ function renderiza_link(link) {
 }
 
 const headers = new Headers();
-headers.append('Authorization', 'token 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', {
+headers.append('Authorization', 'token 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', {
     headers: headers
 });
 
-fetch("https://api.github.com/users/gusssouza", {headers: headers})                                
+fetch("https://api.github.com/users/gusssouza", { headers: headers })
     .then(response => response.json())
     .then(data => {
         console.log(data);
         renderiza_foto(data.avatar_url)
         renderiza_nome(data.name)
         renderiza_link(data.html_url)
-
     })
 
-    .catch(error => {                                                   
+    .catch(error => {
         console.error('algo deu errado na requisição', error);
-
-    }
-
-    )
+    })
 
 function repos(repositorios) {
     const divrepo = document.getElementById('repo');
     divrepo.innerHTML += `
+        <a href=${repositorios.html_url} class="repos" target="_blank">
         <div id="divRepo">
-        <a href=${repositorios.html_url} class="repos" target="_blank">${repositorios.name}</a><br>
-        </div>
+        ${repositorios.name}
+        <br>
+        <h3 class="linguagem"> Linguagem: ${repositorios.language}</h3>
+        </div></a>
         `
 }
-fetch("https://api.github.com/users/gusssouza/repos", {headers: headers})                                
+
+fetch("https://api.github.com/users/gusssouza/repos", { headers: headers })
     .then(response => response.json())
     .then(data => {
         console.log(data);
         data.forEach(repos)
-
-
-    })
+     })
 
 
 
